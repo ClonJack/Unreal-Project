1efcdbf05275fd8b87e315c022f74f4b99358525
Mihail Babichev <54461515+mikle305@users.noreply.github.com>
2024-04-02 16:49:52 +0300

    Fixed outline component filter

--- Assets/_Project/Scripts/GamePlay/Systems/Interaction/OutlineRaycastedObjectsSystem.cs
+++ Assets/_Project/Scripts/GamePlay/Systems/Interaction/OutlineRaycastedObjectsSystem.cs
@@ -7,7 +7,7 @@ namespace UnrealTeam.SB.GamePlay.Systems.Interaction
 {
     public class OutlineRaycastedObjectsSystem : IEcsRunSystem
     {
-        private readonly EcsFilterInject<Inc<RaycastedObjectComponent>> _filter;
+        private readonly EcsFilterInject<Inc<RaycastedObjectComponent, ComponentRef<QuickOutline>>> _filter;
         private readonly EcsPoolInject<RaycastedObjectComponent> _raycastedObjectPool;
         private readonly EcsPoolInject<ComponentRef<QuickOutline>> _outlineRefPool;
         
@@ -20,9 +20,6 @@ namespace UnrealTeam.SB.GamePlay.Systems.Interaction
 
         private void OutlineAndCleanObjects(int raycastedObject)
         {
-            if (!_outlineRefPool.Value.Has(raycastedObject))
-                return;
-
             bool isRaycasted = _raycastedObjectPool.Value.Get(raycastedObject).Raycasted;
             _outlineRefPool.Value.Get(raycastedObject).Component.enabled = isRaycasted;
         }
