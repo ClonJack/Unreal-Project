a07d45a8825a0ddc71f6f2de29e297e42edeb1ff
German Hodun <german.hodun@spherum.io>
2024-04-09 02:56:01 +0300

    update prefab player

--- Assets/_Project/Scripts/GamePlay/Player/PlayerNetwork.cs
+++ Assets/_Project/Scripts/GamePlay/Player/PlayerNetwork.cs
@@ -1,6 +1,8 @@
 ﻿using Fusion;
+using KinematicCharacterController;
 using UnityEngine;
 using UnrealTeam.SB.Common.Ecs.Binders;
+using UnrealTeam.SB.GamePlay.CharacterController.Views;
 using VContainer;
 
 namespace UnrealTeam.SB._Project.Scripts.GamePlay.Player
@@ -9,6 +11,8 @@ namespace UnrealTeam.SB._Project.Scripts.GamePlay.Player
     {
         [SerializeField] private EcsEntityProvider _ecsEntityProvider;
         [SerializeField] private Camera _camera;
+        [SerializeField] private KinematicCharacterMotor _characterMotor;
+        [SerializeField] private CharacterView _characterView;
 
         [Inject]
         public void Construct()
@@ -18,6 +22,9 @@ namespace UnrealTeam.SB._Project.Scripts.GamePlay.Player
                 _ecsEntityProvider.BuildEntity();
 
                 _camera.gameObject.SetActive(true);
+                
+                _characterMotor.enabled = true;
+                _characterView.enabled = true;
             }
         }
     }
